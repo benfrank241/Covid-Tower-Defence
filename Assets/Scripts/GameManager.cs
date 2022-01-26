9bb9d18878dad2415d213b70463dd44a7385971c
bfbartholomew1 <benbart02@gmail.com>
2022-01-26 15:53:08 -0600

    Completed first major merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+//New Code
 public class GameManager : Singleton<GameManager>
 {
     public ObjectPool Pool { get; set; }
 
     public float speed = 0.5f;
 
+    private int currency;
+    private int hp;
+
+    [SerializeField]
+    private Text currencyTxt;
+
+    public int Currency
+    {
+        get
+        {
+            return currency;
+        }
+
+        set
+        {
+            this.currency = value;
+            print(value);
+            this.currencyTxt.text = value.ToString() + "<color=lime>$</color>";
+        }
+    }
+
+    [SerializeField]
+    private Text HPTxt;
+
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+
+        set
+        {
+            this.hp = value;
+            this.HPTxt.text = value.ToString();
+        }
+    }
+
+    //Use this for initialization
+    void Start()
+    {
+        Currency = 5;
+        Hp = 10;
+    }
 
     private void Awake()
     {
@@ -22,7 +67,7 @@ public class GameManager : Singleton<GameManager>
 
     private IEnumerator SpawnWave()
     {
-        //print("runnings");
+        print("runnings");
         string type = "Monster";
 
         Vector3 movement = new Vector3(1, 0, 0);
